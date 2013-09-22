11e4f577a0fb1750178858e0509b731b53792d47
Alexander Boyd <alex@opengroove.org>
2013-09-22 16:10:45 -0600

    Use normal version numbers

--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@ from setuptools import setup
 from subprocess import Popen, PIPE
 from time import gmtime, strftime
 
-timestamp = strftime("%Y.%m.%d.%H.%M.%S", gmtime(int(Popen(["git", "log", "-1", "--format=%ct"], stdout=PIPE).communicate()[0])))
-
 setup(
     name="ttftree",
-    version="0.1." + timestamp,
+    version="0.2.1",
     description="A 2-3 finger tree library for Python",
     author="Alexander Boyd",
     author_email="alex@opengroove.org",
