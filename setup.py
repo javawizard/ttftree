c536a1da8177d6db1130c9b1e39eccb3125d9801
Alexander Boyd <alex@opengroove.org>
2013-09-22 15:49:25 -0600

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from subprocess import Popen, PIPE
+from time import gmtime, strftime
+
+timestamp = strftime("%Y.%m.%d.%H.%M.%S", gmtime(int(Popen(["git", "log", "-1", "--format=%ct"], stdout=PIPE).communicate()[0])))
+
+setup(
+    name="ttftree",
+    version="0.1." + timestamp,
+    description="A 2-3 finger tree library for Python",
+    author="Alexander Boyd",
+    author_email="alex@opengroove.org",
+    modules=["ttftree"]
+)
