21029169961e2d71de6eef53246624a19919689b
Alexander Boyd <alex@opengroove.org>
2013-09-27 01:37:22 -0600

    New version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ttftree",
-    version="0.2.2",
+    version="0.2.3",
     description="A 2-3 finger tree library for Python",
     author="Alexander Boyd",
     author_email="alex@opengroove.org",
